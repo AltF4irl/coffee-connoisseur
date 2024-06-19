6e765dbad6f99886dfde0f18cbff607c08c86e1d
AltF4irl <altf4irl@gmail.com>
2024-06-19 14:19:41 +0300

    Added background and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-purple-951">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Coffee Connoisseur</h1>
+      <h2 className="font-inter">Git fucking Gud</h2>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-ibmplexsans)'],
+        inter: ['var(--font-inter)'],
+      },
+      backgroundImage: {
+        gradient: "url('/static/background.png')",
+      },
       colors: {
         purple: {
-          951: "rgba(79, 70, 229, 1)",
+          951: 'rgba(79, 70, 229, 1)',
         },
         gray: {
-          951: "hsla(0, 0%, 100%, 0.4)",
-          952: "#373b64",
-        }
-      }
+          951: 'hsla(0, 0%, 100%, 0.4)',
+          952: '#373b64',
+        },
+      },
     },
   },
   plugins: [],
