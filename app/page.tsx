1a8500f6337ea4ef925d0efed4e8eb6ef6c5803d
AltF4irl <altf4irl@gmail.com>
2024-06-19 23:01:54 +0300

    Added default store cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
-import Banner from "@/components/banner.client";
-import Image from "next/image";
+import Banner from '@/components/banner.client';
+import Card from '@/components/card.server';
+import { coffeeStores } from '@/lib/tempData';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Banner />
-      <h2 className="font-inter">Git fucking Gud</h2>
+      <div className=''>
+        <h2 className='my-8 text-4xl font-bold text-white'>Istanbul Coffee Shops</h2>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6">
+          {coffeeStores.map((store, index) => (
+            <Card
+              key={index}
+              name={store.name}
+              link={`/coffee-store/${store.name}-${index}`}
+              imageUrl={store.imgUrl}
+            />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
---
+++ components/card.server.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface CardProps {
+  name: string;
+  imageUrl: string;
+  link: string;
+}
+
+export default function Card({ name, imageUrl, link }: CardProps) {
+  return (
+    <Link
+      href={link}
+      className="m-auto rounded-xl border-gray-400 shadow-2xl"
+    >
+      <div
+        className={`glass min-h-[200px] rounded-xl px-5 pb-5 pt-1 backdrop-blur-3xl`}
+      >
+        <div className="my-3">
+          <h2 className="w-64 text-ellipsis whitespace-nowrap text-xl font-bold">
+            {name}
+          </h2>
+        </div>
+        <div className="relative w-full h-48">
+          <Image
+            className="rounded-lg shadow-lg"
+            src={imageUrl}
+            layout="fill"
+            objectFit="cover"
+            alt={`${name} Coffee Store Image`}
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8/+ZNPQAIoQM4xp5zkgAAAABJRU5ErkJggg=="
+            placeholder="blur"
+          />
+        </div>
+      </div>
+    </Link>
+  );
+}
